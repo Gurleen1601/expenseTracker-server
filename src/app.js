fd46f58cc9bbbf5b3891b496b41d80c2970fbc74
Gurleen1601 <gurleen0790.cse19@chitkara.edu.in>
2022-06-05 17:08:13 +0530

    different server repo

---
+++ src/app.js
@@ -0,0 +1,39 @@
+// require express for setting up the express server
+const express = require('express');
+
+//requiring the registerUser function from userCtrl.js to pass in the post req.
+const {registerUser} = require('./controllers/users/usersCtrl');
+const {errorHandler,notFound} = require("./middlewares/errorMiddleware");
+const userRoute = require('./routes/users/usersRoute');
+const dotenv=require("dotenv");
+//importing the database into the server
+const dbConnect = require('./config/dbConnect');
+
+
+// using express
+const app = express();
+
+//env
+dotenv.config();
+
+//connecting the db
+dbConnect();
+
+
+//middlewares
+app.use(express.json());
+
+app.get('/',(req,res) => {
+    res.json({msg:"welcome"});
+})
+
+//users routes
+app.use('/api/users',userRoute);
+
+//error
+app.use(notFound);
+app.use(errorHandler);
+
+
+
+module.exports=app;
